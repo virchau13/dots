ebe1d8bc30d59b652baf3b31c7454049927c86b2
Vir Chaudhury <virchau13@hexular.net>
2021-05-22 19:28:10 +0800

    chore: fix formatting of lsp.lua

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -107,42 +107,29 @@ local on_attach = function(client, bufnr)
     -- Mappings.
     local opts = {noremap = true, silent = true}
 
-    buf_set_keymap('n', '<leader>ca', '<Cmd>lua vim.lsp.buf.code_action()<CR>',
-                   opts)
+    buf_set_keymap('n', '<leader>ca', '<Cmd>lua vim.lsp.buf.code_action()<CR>', opts)
     buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
     buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
     buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
     buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>',
-                   opts)
+    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
     buf_set_keymap('n', '<space>wa',
                    '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-    buf_set_keymap('n', '<space>wr',
-                   '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-    buf_set_keymap('n', '<space>wl',
-                   '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
-                   opts)
-    buf_set_keymap('n', '<space>D',
-                   '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+    buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
     buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
     buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    buf_set_keymap('n', '<space>e',
-                   '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>',
-                   opts)
-    buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>',
-                   opts)
-    buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>',
-                   opts)
-    buf_set_keymap('n', '<space>q',
-                   '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+    buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+    buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+    buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+    buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
 
     -- Set some keybinds conditional on server capabilities
     if client.resolved_capabilities.document_formatting then
-        buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>",
-                       opts)
+        buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
     elseif client.resolved_capabilities.document_range_formatting then
-        buf_set_keymap("n", "<space>f",
-                       "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+        buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
     end
 
 end
@@ -181,20 +168,27 @@ local settings = {
                 procMacro = {enable = true}
             }
         }
-    }, 
+    },
     -- omnisharp
-    {cmd = {'omnisharp', '--languageserver'}}, -- cmake
-    {}, 
+    {
+        cmd = {'omnisharp', '--languageserver'}
+    },
+    -- cmake
+    {},
     -- texlab
     {},
     -- jdtls
-    {}, 
+    {},
     -- bashls
-    {}, 
+    {},
     -- html
-    {cmd = {'vscode-html-languageserver', '--stdio'}}, 
+    {
+        cmd = {'vscode-html-languageserver', '--stdio'}
+    },
     -- haxe_language_server
-    {cmd = {'node', '~/prog/repos/haxe-language-server/bin/server.js'}},
+    {
+        cmd = {'node', '~/prog/repos/haxe-language-server/bin/server.js'}
+    },
     -- sumneko_lua
     {
         cmd = {"lua-language-server"},
@@ -219,15 +213,23 @@ local settings = {
                 }
             }
         }
-    }, -- glsl
+    },
+    -- glsl
     {}
 }
 
 -- Merges ...dictionaries? objects? tables?
-function merge(a, b) for k, v in pairs(b) do a[k] = v end end
+function merge(a, b)
+    for k, v in pairs(b) do
+        a[k] = v
+    end
+end
 
 for i, lsp in ipairs(servers) do
-    setup_obj = {on_attach = on_attach, capabilities = capabilities}
+    setup_obj = {
+        on_attach = on_attach,
+        capabilities = capabilities
+    }
     merge(setup_obj, settings[i])
     nvim_lsp[lsp].setup(setup_obj)
 end

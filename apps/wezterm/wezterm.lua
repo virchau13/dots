bdb18d3552f1ef54b550488cd55e8be5a8029e40
Vir Chaudhury <virchau13@hexular.net>
2022-08-05 22:11:27 +0800

    work on wezterm config

--- apps/wezterm/wezterm.lua
+++ apps/wezterm/wezterm.lua
@@ -1,3 +1,10 @@
+local wezterm = require 'wezterm'
+
 return {
     harfbuzz_features = {"calt=0", "clig=0", "liga=0"},
+    freetype_load_target = "HorizontalLcd",
+    hide_tab_bar_if_only_one_tab = true,
+    font = wezterm.font('Hex Mono'),
+    font_size = 11.0,
+    color_scheme = 'tokyonight',
 }
